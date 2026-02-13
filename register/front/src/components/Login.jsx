2d5d8af782c8af53a29bde6246d02de14a0e063f
youminsu0523 <youminsu0523@gmail.com>
2026-02-13 18:34:51 +0900

    feat: implement basic login and registration UI components.

--- register/front/src/components/Login.jsx
+++ register/front/src/components/Login.jsx
@@ -3,31 +3,41 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>
-      <div>
-        <h2>Login</h2>
+    <div className="flex justify-center items-center h-screen">
+      <div className="shadow-lg px-8 py-5 border w-100 rounded-md">
+        <h2 className="text-lg font-bold mb-4">Login</h2>
         <form>
-          <div>
-            <label htmlFor="email">Email</label>
+          <div className="mb-4">
+            <label htmlFor="email" className="text-gray-700">
+              Email
+            </label>
             <input
               type="email"
               placeholder="Enter Your Email..."
               name="email"
+              className="w-full px-3 py-2 border"
             />
           </div>
-          <div>
-            <label htmlFor="password">Password</label>
+          <div className="mb-4">
+            <label htmlFor="email" className="text-gray-700">
+              Password
+            </label>
             <input
               type="password"
               placeholder="Enter Your Password..."
               name="password"
+              className="w-full px-3 py-2 border"
             />
           </div>
-          <button type="submit">Login</button>
+          <button type="submit" className="w-full bg-green-500 text-white py-2">
+            Login
+          </button>
         </form>
-        <div>
-          <span>Still Don't Have Account ?</span>
-          <Link to="/register">Sign Up</Link>
+        <div className="text-center mt-4">
+          <span className="text-gray-700">Don't Have Account? | </span>
+          <Link to="/register" className="text-blue-500 underline">
+            Sign Up
+          </Link>
         </div>
       </div>
     </div>
--- register/front/src/components/Register.jsx
+++ register/front/src/components/Register.jsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="shadow-lg px-8 py-5 border w-100 rounded-md">
+        <h2 className="text-lg font-bold mb-4">Register</h2>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="username" className="text-gray-700">
+              Username
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Your Username..."
+              name="username"
+              className="w-full px-3 py-2 border"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Your Email..."
+              name="email"
+              className="w-full px-3 py-2 border"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Your Password..."
+              name="password"
+              className="w-full px-3 py-2 border"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="profile_image" className="text-gray-700">
+              Profile Image
+            </label>
+            <input type="file" name="profile_image" className="w-full" />
+          </div>
+          <button type="submit" className="w-full bg-green-500 text-white py-2">
+            Sign Up
+          </button>
+        </form>
+        <div className="text-center mt-4">
+          <span className="text-gray-700">Already Have Account? | </span>
+          <Link to="/login" className="text-blue-500 underline">
+            Sign in
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
