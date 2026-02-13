73cec1c416010cf8d1388d0e8e06e39694c7afc7
youminsu0523 <youminsu0523@gmail.com>
2026-02-13 18:12:57 +0900

    feat: Add Login component with form and Header component with login navigation.

--- register/front/src/components/Header.jsx
+++ register/front/src/components/Header.jsx
@@ -3,21 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div>
+      <Link to="/login">Login</Link>
+    </div>
   );
 };
 
--- register/front/src/components/Login.jsx
+++ register/front/src/components/Login.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <div>
+        <h2>Login</h2>
+        <form>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              placeholder="Enter Your Email..."
+              name="email"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="Enter Your Password..."
+              name="password"
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <div>
+          <span>Still Don't Have Account ?</span>
+          <Link to="/register">Sign Up</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
