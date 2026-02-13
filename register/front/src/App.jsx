e96e1f06c98ea8a94ef79e243c63b2fa03d2d27f
youminsu0523 <youminsu0523@gmail.com>
2026-02-13 18:12:42 +0900

    feat: Initialize a new React frontend application with Vite, React Router, Tailwind CSS, and basic components for home, login, and registration.

---
+++ register/front/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Register from './components/Register';
+import Login from './components/Login';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
